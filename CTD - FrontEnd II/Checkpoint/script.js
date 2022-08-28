1fceea3d37aa419e9b54a6109011e6b95512b92e
filiperslima <frodrigues144@gmail.com>
2022-08-28 01:29:01 -0300

    deixei mais ou menos

--- CTD - FrontEnd II/Checkpoint/script.js
+++ CTD - FrontEnd II/Checkpoint/script.js
@@ -1,26 +1,37 @@
 
 window.onload = function () {
-const cardArea = document.getElementById('card-area');
+    let scrollPositionX = 0;
+    let scrollPositionY = 0;
 
-const imageUrl = document.querySelector('#url');
-const title = document.getElementById('title');
-const textArea = document.querySelector('textarea');
-const button = document.querySelector('button')
-button.addEventListener('click', sendInformation)
-const savedData = []
+    const cardArea = document.querySelector('.card-area');
 
-function clearFields() {
-    textArea.value= "";
-    title.value = "";
-    imageUrl.value = "";
-}
+    const imageUrl = document.querySelector('#url');
+    const title = document.getElementById('title');
+    const textArea = document.querySelector('textarea');
+    const button = document.querySelector('button')
+    
+
+    const primaryArea = document.querySelector("#primary-area")
+    const secondaryArea = document.querySelector("#secondary-area")
+    let toPrimary = true;
+
+    const right = document.querySelectorAll(".right")
+    const left = document.querySelectorAll(".left")
+    right[0].addEventListener("mousedown", scrollingX)
+    left[0].addEventListener("mousedown", scrollingY)
+    right[1].addEventListener("mousedown", scrollingX)
+    left[1].addEventListener("mousedown", scrollingY)
 
 
-function createCards (url, title, description) {
-    const divCard = document.createElement("div");
-    divCard.setAttribute('class', 'card')
-    divCard.innerHTML = 
-                `
+    button.addEventListener('click', sendInformation)
+    const savedData = []
+
+
+    function createCards(url, title, description) {
+        const divCard = document.createElement("div");
+        divCard.setAttribute('class', 'card')
+        divCard.innerHTML =
+            `
                 <div class="face face1">
                 <div class="content">
                     <img src=${url}>
@@ -33,22 +44,57 @@ function createCards (url, title, description) {
                 </div>
                 </div>
 `
-cardArea.appendChild(divCard)
+        if(toPrimary){
+            primaryArea.appendChild(divCard)
+        }
+        else {
+            secondaryArea.appendChild(divCard);
+        }
+        toPrimary = !toPrimary
 
-}
-
-function sendInformation (event) {
-    if (title.value != "" && textArea.value != "" && imageUrl.value !=""){
-    let toSave = {
-        name: title.value,
-        description: textArea.value,
-        url: imageUrl.value
     }
-    savedData.push(toSave)
-    createCards(toSave.url, toSave.name, toSave.description)
-    clearFields()
-}
-event.preventDefault()
+    for (let index = 0; index < 8; index++) {
+        createCards("https://i.ytimg.com/vi/mDGV5UucTu8/maxresdefault.jpg", "doutor estranho 2", "Um filme aleatório ai")
 
-}
+    }
+    function scrollingX(event) {
+        if (scrollPositionX / 2 <= event.currentTarget.parentElement.scrollHeight) {
+            scrollPositionX += 100;
+            scrollPositionY -= 100;
+
+            console.log(event.currentTarget.parentElement.scrollHeight);
+            console.log(scrollPositionY);
+            event.currentTarget.parentElement.scroll(scrollPositionX, scrollPositionY)
+        }
+    }
+    function scrollingY(event) {
+        if (scrollPositionY / 2 <= !event.currentTarget.parentElement.scrollHeight) {
+
+            scrollPositionY += 100;
+            scrollPositionX -= 100;
+            console.log(event.currentTarget.parentElement.scrollHeight);
+            console.log(scrollPositionY);
+            event.currentTarget.parentElement.scroll(scrollPositionX, scrollPositionY)
+        }
+
+
+
+    }
+
+    function sendInformation(event) {
+        if (title.value != "" && textArea.value != "" && imageUrl.value != "") {
+            let toSave = {
+                name: title.value,
+                description: textArea.value,
+                url: imageUrl.value
+            }
+            savedData.push(toSave)
+            createCards(toSave.url, toSave.name, toSave.description)
+            document.forms[0].reset()
+            scrollBar()
+        }
+        event.preventDefault()
+
+    }
+
 }
