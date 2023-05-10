3e772268efc942e475a150b346f675c1ff2b59cb
filiperslima <98400757+filiperslima@users.noreply.github.com>
2023-05-10 03:33:45 -0300

    mesa-02

---
+++ mesa-2/mesa-02/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BlinkGif from './blink.gif';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Olá mundo!!!</h1>
+     <img src={BlinkGif}></img>
+    </div>
+    
+  );
+}
+
+export default App;
