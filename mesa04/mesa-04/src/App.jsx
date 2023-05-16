fd122af50cd1203a96c392ef822dded090de4da7
filiperslima <98400757+filiperslima@users.noreply.github.com>
2023-05-16 00:52:02 -0300

    mes 04

---
+++ mesa04/mesa-04/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import HelloComponent from './components/HelloComponent'
+
+function App() {
+  
+
+  return (
+    <>
+    <HelloComponent
+    nome="Filipe"/>
+    <h2>Seja muito bem-vindo!</h2>
+    </>
+    
+
+    
+  )
+}
+
+export default App
